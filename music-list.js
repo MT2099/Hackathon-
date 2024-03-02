c7fa51e490c6483f902819463865359f1e4a066a
MT <149652607+MT2099@users.noreply.github.com>
2024-03-03 03:43:56 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,110 @@
+let allMusic = [
+    {
+        name: "Youngblood",
+        artist: "5 Seconds of Summer",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "If We Have Eachother",
+        artist: "Alec Benjamin",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Automotivo Bibi Fogosa",
+        artist: "Bibi",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Light switch",
+        artist: "Charlie Puth",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Hands Up",
+        artist: "City Wolf",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Levitation",
+        artist: "Dua Lipa",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Guy.exe",
+        artist: "SUPERFRUIT",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "bling-blang-blang-born",
+        artist: "Creepy Nuts",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Name Tag",
+        artist: "Nivicious",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Never Let This Go",
+        artist: "Tom Frane",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Plain Jane",
+        artist: "Kean Dysso",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Sunflower",
+        artist: "Post Malone",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Gangnam Style",
+        artist: "PSY",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Stitches",
+        artist: "Shawn Mendes",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "Level",
+        artist: "SawanoHiroyuki",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "Fallin'(Adrenaline)",
+        artist: "Why Don't We",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "Wannabe",
+        artist: "Why mona",
+        img: "music-17",
+        src: "music-17"
+    },
+    {
+        name: "Falling Into The Night",
+        artist: "Yaosabi",
+        img: "music-18",
+        src: "music-18"
+    },
+];
